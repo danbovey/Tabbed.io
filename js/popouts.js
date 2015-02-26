d8f7c6c043b9c3fdedb4ccdf239121a2787263f5
Dan Bovey <dan@danbovey.uk>
2015-02-26 19:41:48 +0000

    Fixed popouts

--- js/popouts.js
+++ js/popouts.js
@@ -6,20 +6,20 @@ $(function() {
 	$('.popout').click(function(e) {
 		e.stopPropagation();
 	});
-
-	function popout(name) {
-		if(name != undefined) {
-			var pop = $('#' + name);
-
-			$('.popout').not(document.getElementById(name)).removeClass('active');
-
-			if(pop.hasClass('active')) {
-				pop.removeClass('active');
-			} else {
-				pop.addClass('active');
-			}
-		} else {
-			$('.popout').removeClass('active');
-		}
-	}
 });
+
+function popout(name) {
+	if(name != undefined) {
+		var pop = $('#' + name);
+
+		$('.popout').not(document.getElementById(name)).removeClass('active');
+
+		if(pop.hasClass('active')) {
+			pop.removeClass('active');
+		} else {
+			pop.addClass('active');
+		}
+	} else {
+		$('.popout').removeClass('active');
+	}
+}
